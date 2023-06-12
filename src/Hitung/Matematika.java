45958399cdc07ca1eae85bf55c29f2f98564cda2
unknown <delvikarmilasari@gmail.com>
2023-06-12 21:15:18 +0800

    Membuat constructor dan setPenjumlahan

--- src/Hitung/Matematika.java
+++ src/Hitung/Matematika.java
@@ -15,10 +15,7 @@ public class Matematika {
     
  public  Matematika (double bil1, double bil2){
      this.bil1=bil1;
-     this.bil2=bil2;
-   
-    
-    
+     this.bil2=bil2;   
  }
 
     public double setPenjumlahan() {
@@ -32,6 +29,10 @@ public class Matematika {
     }
     public double setPembagian() {
         return bil1/bil2;
+    }
+    
+    public double setPerkalian2() {
+        return bil1 * bil2;
     }
     
 }
