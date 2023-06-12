d325e77458f400cf42d75763abc49a477143703c
unknown <delvikarmilasari@gmail.com>
2023-06-12 21:00:45 +0800

    Membuat class matematika beraksi

--- src/Hitung/Matematika.java
+++ src/Hitung/Matematika.java
@@ -10,5 +10,28 @@ package Hitung;
  * @author acer
  */
 public class Matematika {
+   
+    double bil1,bil2;
+    
+ public  Matematika (double bil1, double bil2){
+     this.bil1=bil1;
+     this.bil2=bil2;
+   
+    
+    
+ }
+
+    public double setPenjumlahan() {
+        return bil1+bil2;
+    }
+    public double setPengurangan() {
+        return bil1-bil2;
+    }
+    public double setPerkalian() {
+        return bil1*bil2;
+    }
+    public double setPembagian() {
+        return bil1/bil2;
+    }
     
 }
--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Hitung;
 
-/**
- *
- * @author acer
- */
+
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika delvi= new Matematika(5,9);
+      
+     System.out.println("Penjumlahan: "+ delvi.setPenjumlahan());
+     System.out.println("Pengurangan: "+ delvi.setPengurangan());
+     System.out.println("Perkalian:"+ delvi.setPerkalian());
+     System.out.println("Pembagian:"+ delvi.setPembagian());
+    }
 }
